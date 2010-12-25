fe3e4b803fc490aee76f341a5fff4a84f53b38a2
arekt <arekt@turlewicz.com>
2010-12-25 21:50:37 +0000

    Next step: mongomapper handler

--- import_edict_to_mongo.rb
+++ import_edict_to_mongo.rb
@@ -1,20 +1,32 @@
 require "rubygems"
 require "kakasi"
+require "iconv"
+
 # tail -n 2 /usr/share/edict/edict | kakasi -ieuc -Ha
+
+class String
+  def ujis
+    Iconv.iconv("UJIS","UTF-8",self)
+  end
+  def utf8
+    Iconv.iconv("UTF-8","UJIS",self)
+  end
+  def romaji
+    Kakasi::kakasi("-ieuc -s -Ka -Ja -Ha", self)  #expect UJIS/EUC string
+  end
+end
 
 
 class Importer
-  def self.romaji(h)
-      Kakasi::kakasi("-ieuc -s -Ka -Ja -Ha", h)
-  end
-
   def self.import(file_name)
       file = open(file_name)
       header =  file.readline
       file.each do |line|
-          if /\[(.*)\]/.match(line) then
-              hiragana = $1
-              puts romaji(hiragana)
+          if /(.*)\[(.*)\].*[\/](.*)[\/]/.match(line) then
+              kanji = $1
+              hiragana = $2
+              description = $3
+              puts "k| #{kanji.utf8}| r| #{hiragana.romaji}| h| #{hiragana.utf8} description: #{$3}"
           end
       end
   end
