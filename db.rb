bd20a333ffd910f0c66182eeb095a357148f1a45
arekt <arekt@turlewicz.com>
2010-12-27 15:12:29 +0000

    Added options for read db details from ENV

--- db.rb
+++ db.rb
@@ -4,7 +4,12 @@ require 'pp'
 
 module DB
   def self.database_initialize(name)
-    MongoMapper.connection = Mongo::Connection.new('localhost',27017)
+    if ENV['DB_HOST']
+      MongoMapper.connection = Mongo::Connection.new(ENV['DB_HOST'],27017)
+      MongoMapper.database.authenticate(ENV['DB_USER'], ENV['DB_PASS'])
+    else
+      MongoMapper.connection = Mongo::Connection.new('localhost',27017)
+    end
     MongoMapper.database = name
  end
   class Tag
