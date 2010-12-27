0cb0ddcf9d8ef05ee515a33b3641b516506b8c2f
arekt <arekt@turlewicz.com>
2010-12-27 15:13:27 +0000

    Added limit for 20 items

--- sinatra_server.rb
+++ sinatra_server.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'db'
+require 'erb'
 
 DB.database_initialize('edict');
 
@@ -10,13 +11,27 @@ end
 
 get '/edict/*' do
     response ="" 
-    @words = DB::Word.all :romaji => Regexp.new(params[:splat][0])
-    if !@words.empty? then
+    case params[:splat][0].slice!(/\.xml/)
+    when '.xml' then
+      @words = DB::Word.all :romaji => Regexp.new(params[:splat][0]), :limit => 20
+      template = ERB.new <<-EOF
+<?xml version="1.0" encoding="UTF-8" ?>
+<catalog>
+  <% @words.each do |word| %>
+  <title>
+    <name><%=word.kanji%></name>
+    <description><%= word.hiragana %>: <%= word.desc %></description>
+  </title>
+  <% end %>
+</catalog>
+EOF
+      response=template.result(binding)
+      puts template.result(binding) 
+    else 
+      @words = DB::Word.all :romaji => Regexp.new(params[:splat][0]), :limit => 20
       @words.each do |word|
-      response += "#{word.kanji} #{word.hiragana} \n#{word.desc}<br>"
+        response += "#{word.kanji} #{word.hiragana} \n#{word.desc}<br>"
       end
-    else
-      "Not found"
     end
     response
 end
